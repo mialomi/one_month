05f5c13f9b9896cfab190e67d1a6e06e3391a4de
Mialomi <e.silvasm@gmail.com>
2023-10-24 13:59:58 +0200

    EventRequest validation

---
+++ app/Http/Requests/EventRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            
+            'brands_id' => 'required',
+            'seasons_id' => 'required',
+            'collections_id' => 'required',
+            'places_id' => 'required',
+            'date_time' => 'required|date',
+            'online_events' => 'nullable|integer',
+            'location'  => 'string|max:30',
+            'spendings' => 'nullable|numeric',
+            'earnings' => 'nullable|numeric',
+           // 'money_id'  => 'nullable',
+        ];
+    }
+
+    public function messages(): array{
+        return [
+            'brands_id.required' => 'Choose a brand from the list!',
+            'seasons_id.required' => 'Choose a season from the list!',
+            'collections_id.required' => 'Choose a collection from the list!',
+            'places_id.required' => 'Choose the city where the show will take place!',
+            'spendings.numeric' => 'Hey, this is not a number!',
+            'earnings.numeric' => 'Hey, this is not a number!',
+            'date_time.required' => 'The when is crucial for the show!',
+            'location.required_if' => 'Is it a building, museum, square... where?',
+
+
+        ];
+    }
+}    
